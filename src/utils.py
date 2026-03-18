4bf5d7ba4c9c83f7c00f04525edc7ac19b2f46ab
Jonathan Sabbe <sabbe.jonathan@gmail.com>
2026-03-18 15:25:07 +0100

    feat: add option that copy password to system clipboard

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,19 @@
 from hashlib import sha256
 from random import choice
 from string import digits, ascii_lowercase
+from sys import stdin
 from bcrypt import hashpw, gensalt
+from getpass import getpass
+from argparse import ArgumentParser, Namespace
+from pyperclip import copy as pyperclip_copy
 
+from config import DEFAULT_SIZE
+
+def get_typed_password() -> str:
+    return getpass()
+
+def get_stdin_password() -> str:
+    return stdin.readline().strip()
 
 def generate_password(size: int, characters: str) -> str:
     return "".join([choice(characters) for _ in range(size)])
@@ -31,3 +42,20 @@ def show_results(results: dict[str, str]) -> None:
 
     for k, v in dict(sorted(results.items())).items():
         print(f"{k.title():<{max_length}}: {v}")
+
+def get_parser_args() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument("--size", type=int, default=DEFAULT_SIZE)
+    parser.add_argument("--from-stdin", action="store_true")
+    parser.add_argument("--ask-password", action="store_true")
+    parser.add_argument("--show-password", action="store_true")
+    parser.add_argument("--apple-style", action="store_true")
+    parser.add_argument("--bcrypt", action="store_true")
+    parser.add_argument("--sha256", action="store_true")
+    parser.add_argument("--clipboard", action="store_true")
+
+    return parser.parse_args()
+
+def copy_password_clipboard(password) -> None:
+    pyperclip_copy(password)
